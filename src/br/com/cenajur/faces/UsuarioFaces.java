ff5586ac1b36d3514ac0d8ebdc51f9b1748a2ef9
Andre Topazio <andre.topazio@gmail.com>
2012-07-04 23:46:28 -0300

    first commit

--- src/br/com/cenajur/faces/UsuarioFaces.java
+++ src/br/com/cenajur/faces/UsuarioFaces.java
@@ -1,72 +1,65 @@
 package br.com.cenajur.faces;
 
-import java.io.Serializable;
 import java.util.Collections;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
 import br.com.cenajur.model.Usuario;
 import br.com.topsys.exception.TSApplicationException;
-
-
+import br.com.topsys.web.faces.TSMainFaces;
 
 @ManagedBean
 @RequestScoped
-public class UsuarioFaces implements Serializable  {
-	
-     
+public class UsuarioFaces extends TSMainFaces {
+
 	private Usuario usuario;
 	private List<Usuario> usuarios;
-   
-    public UsuarioFaces() {
-       this.usuario = new Usuario();
-       this.usuarios = Collections.emptyList();
-       
-    }
 
-	
-	public void salvar() {
-		try {
-			
-			
-			usuario.save();
-			
-			this.usuarios = this.usuario.findAll();
-			
-						
-		} catch (TSApplicationException e) {
-			
-			e.printStackTrace();
-		}
+	public UsuarioFaces() {
+		this.usuarios = Collections.emptyList();
+	}
+
+	@Override
+	protected String insert() throws TSApplicationException {
+
+		usuario.save();
+
+		this.usuarios = this.usuario.findAll();
+		
+		return null;
 	}
 	
-	public void pesquisar(){
+	public void pesquisar() {
 		this.usuarios = this.usuario.findByModel();
 	}
+	
+	
+	@Override
+	@PostConstruct
+	protected void clearFields() {
+		this.usuario = new Usuario();
+		
+	}
 
+	
 
 	public Usuario getUsuario() {
 		return usuario;
 	}
 
-
 	public void setUsuario(Usuario usuario) {
 		this.usuario = usuario;
 	}
-
 
 	public List<Usuario> getUsuarios() {
 		return usuarios;
 	}
 
-
 	public void setUsuarios(List<Usuario> usuarios) {
 		this.usuarios = usuarios;
 	}
-
-
-	
 
 }
