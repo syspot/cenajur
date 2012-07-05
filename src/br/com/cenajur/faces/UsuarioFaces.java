e68b07b616be4027198d2a27c562887e2647b887
Andre Topazio <andre.topazio@gmail.com>
2012-07-04 23:23:18 -0300

    first commit

--- src/br/com/cenajur/faces/UsuarioFaces.java
+++ src/br/com/cenajur/faces/UsuarioFaces.java
@@ -1,20 +1,28 @@
 package br.com.cenajur.faces;
 
+import java.io.Serializable;
+import java.util.Collections;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 
 import br.com.cenajur.model.Usuario;
 import br.com.topsys.exception.TSApplicationException;
 
 
 
-@ManagedBean()
-public class UsuarioFaces  {
+@ManagedBean
+@RequestScoped
+public class UsuarioFaces implements Serializable  {
 	
      
 	private Usuario usuario;
+	private List<Usuario> usuarios;
    
     public UsuarioFaces() {
        this.usuario = new Usuario();
+       this.usuarios = Collections.emptyList();
        
     }
 
@@ -24,6 +32,8 @@ public class UsuarioFaces  {
 			
 			
 			usuario.save();
+			
+			this.usuarios = this.usuario.findAll();
 			
 						
 		} catch (TSApplicationException e) {
@@ -40,6 +50,16 @@ public class UsuarioFaces  {
 
 	public void setUsuario(Usuario usuario) {
 		this.usuario = usuario;
+	}
+
+
+	public List<Usuario> getUsuarios() {
+		return usuarios;
+	}
+
+
+	public void setUsuarios(List<Usuario> usuarios) {
+		this.usuarios = usuarios;
 	}
 
 
