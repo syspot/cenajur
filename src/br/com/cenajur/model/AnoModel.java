074861cf71092fe9b36269586c0d4031298e45fb
Fagner <Fagner@DESENV-NOTE.grupoatarde.com.br>
2012-10-24 23:49:26 -0300

    .

---
+++ src/br/com/cenajur/model/AnoModel.java
@@ -0,0 +1,110 @@
+package br.com.cenajur.model;
+
+import java.math.BigInteger;
+
+public class AnoModel {
+
+	private String descricao;
+	
+	private BigInteger jan;
+	private BigInteger fev;
+	private BigInteger mar;
+	private BigInteger abr;
+	private BigInteger mai;
+	private BigInteger jun;
+	private BigInteger jul;
+	private BigInteger ago;
+	private BigInteger set;
+	private BigInteger out;
+	private BigInteger nov;
+	private BigInteger dez;
+	
+	private BigInteger tot;
+	
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public BigInteger getJan() {
+		return jan;
+	}
+	public void setJan(BigInteger jan) {
+		this.jan = jan;
+	}
+	public BigInteger getFev() {
+		return fev;
+	}
+	public void setFev(BigInteger fev) {
+		this.fev = fev;
+	}
+	public BigInteger getMar() {
+		return mar;
+	}
+	public void setMar(BigInteger mar) {
+		this.mar = mar;
+	}
+	public BigInteger getAbr() {
+		return abr;
+	}
+	public void setAbr(BigInteger abr) {
+		this.abr = abr;
+	}
+	public BigInteger getMai() {
+		return mai;
+	}
+	public void setMai(BigInteger mai) {
+		this.mai = mai;
+	}
+	public BigInteger getJun() {
+		return jun;
+	}
+	public void setJun(BigInteger jun) {
+		this.jun = jun;
+	}
+	public BigInteger getJul() {
+		return jul;
+	}
+	public void setJul(BigInteger jul) {
+		this.jul = jul;
+	}
+	public BigInteger getAgo() {
+		return ago;
+	}
+	public void setAgo(BigInteger ago) {
+		this.ago = ago;
+	}
+	public BigInteger getSet() {
+		return set;
+	}
+	public void setSet(BigInteger set) {
+		this.set = set;
+	}
+	public BigInteger getOut() {
+		return out;
+	}
+	public void setOut(BigInteger out) {
+		this.out = out;
+	}
+	public BigInteger getNov() {
+		return nov;
+	}
+	public void setNov(BigInteger nov) {
+		this.nov = nov;
+	}
+	public BigInteger getDez() {
+		return dez;
+	}
+	public void setDez(BigInteger dez) {
+		this.dez = dez;
+	}
+	public BigInteger getTot() {
+		return tot;
+	}
+	public void setTot(BigInteger tot) {
+		this.tot = tot;
+	}
+	
+}
